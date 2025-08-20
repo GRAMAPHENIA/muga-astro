c14582e5ff7188d217a7052903d13b10c8e42eda
gramaphenia <dicoratojuanpablo@gmail.com>
2025-08-19 21:40:22 -0300

    build: actualizar archivos de configuración y simplificar README
    - Actualizar tsconfig.json para refinar las rutas incluidas
    - Agregar paleta de colores personalizada en tailwind.config.js
    - Reemplazar el README por contenido específico del proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        muga: {
+          primary: '#FF5353',
+          dark: {
+            900: '#191717',
+            800: '#2D1F1F',
+            700: '#302E2E',
+          },
+          neutral: '#D9D9D9',
+          white: '#FFFFFF',
+        },
+      },
+    },
   },
   plugins: [],
 }
