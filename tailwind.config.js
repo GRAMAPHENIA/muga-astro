9523161aecc40811d8f38ce63c6f062fa2ce596c
gramaphenia <dicoratojuanpablo@gmail.com>
2025-08-21 06:52:52 -0300

    feat(ui): implementar nuevos componentes de navegación y migas de pan
    - Añadir barra de navegación responsive con efectos de desplazamiento y resaltado de enlaces activos
    - Crear componente de migas de pan para una mejor navegación entre páginas
    - Actualizar la estructura del layout para incluir navegación y migas de pan
    - Añadir nuevas páginas (productos, portafolio, características, nosotros, contacto)
    - Mejorar estilos globales y variables de color
    - Añadir transiciones suaves y efectos al pasar el cursor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      colors: {
-        muga: {
-          primary: '#FF5353',
-          dark: {
-            900: '#191717',
-            800: '#2D1F1F',
-            700: '#302E2E',
-          },
-          neutral: '#D9D9D9',
-          white: '#FFFFFF',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
